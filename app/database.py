0506860d73cc9d8fec88be53c454b0a8ed2c83eb
Harshal Prajapati <your-harshalpraja2004@gmail.com>
2026-01-13 17:29:15 +0530

    Dockerize TaskFlow application

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://taskflow_user:taskflow_pass@localhost/taskflow_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://taskflow_user:taskflow_pass@localhost/taskflow_db"
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
 
